d618ceda245bf79ace7a2f53ba21b4d9cc3a5784
leofelicitas <52071878+leofelicitas@users.noreply.github.com>
2020-11-30 17:37:45 +0100

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,11 @@
 // migrating the appropriate contracts
 
-//check file names of .sol roles
-
-var FarmerRole = artifacts.require("./Farmer.sol");
-var DistributorRole = artifacts.require("./Distributor.sol");
-var ManufacturerRole = artifacts.require("./Manufacturer.sol");
-var RetailerRole = artifacts.require("./Retailer.sol");
-var ConsumerRole = artifacts.require("./Consumer.sol");
-var SupplyChain = artifacts.require("./chain.sol");
+var FarmerRole = artifacts.require("Finance-for-big-data/accesscontrol/Farmer.sol");
+var DistributorRole = artifacts.require("Finance-for-big-data/accesscontrol/Distributor.sol");
+var ManufacturerRole = artifacts.require("Finance-for-big-data/accesscontrol/Manufacturer.sol");
+var RetailerRole = artifacts.require("Finance-for-big-data/accesscontrol/Retailer.sol");
+var ConsumerRole = artifacts.require("Finance-for-big-data/accesscontrol/Consumer.sol");
+var SupplyChain = artifacts.require("Finance-for-big-data/supplychain/chain.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(FarmerRole);
